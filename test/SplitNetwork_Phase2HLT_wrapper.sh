2646b264e966de65dd0b1e82605b3c01c36f9bb7
Christian Veelken <christian.veelken@cern.ch>
2020-08-26 10:45:46 +0300

    moved training scripts for Phase-2 HLT into separate subdirectory

--- test/SplitNetwork_Phase2HLT_wrapper.sh
+++ test/SplitNetwork_Phase2HLT_wrapper.sh
@@ -1,3 +1,4 @@
-cd $CMSSW_BASE/src/TauMLTools/Training/python/2017v2
+cd $CMSSW_BASE/src/TauMLTools/Training/python/Phase2HLTv2
+echo "executing command = 'python SplitNetwork_Phase2HLT.py --input $1 --model $2'"
 python SplitNetwork_Phase2HLT.py --input $1 --model $2
 cd -
--- test/Training_p6_Phase2HLT_wrapper.sh
+++ test/Training_p6_Phase2HLT_wrapper.sh
@@ -1,3 +1,4 @@
-cd $CMSSW_BASE/src/TauMLTools/Training/python/2017v2
+cd $CMSSW_BASE/src/TauMLTools/Training/python/Phase2HLTv2
+echo "executing command = 'Training_p6_Phase2HLT.py --input $1 --model $2'"
 python Training_p6_Phase2HLT.py --input $1 --model $2
 cd -
